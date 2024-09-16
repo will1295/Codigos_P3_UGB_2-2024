cef1bf502095c30b89d43e2b296f46c2353a9fae
willian <willian120795@gmail.com>
2024-09-16 13:50:07 -0600

    agregando codigo

---
+++ B1/semana9/email1.py
@@ -0,0 +1,22 @@
+import smtplib
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+#Reemplazar variables de entorno con tus valores
+servidor = os.getenv('SERVIDOR')
+puerto = os.getenv('PUERTO')
+correo = os.getenv('CORREO')
+contr = os.getenv('CONTRA')
+
+try:
+    conec = smtplib.SMTP(servidor,puerto)
+    conec.starttls()
+    conec.login(correo,contr)
+    conec.sendmail(correo,correo,"Subject: Este es el asunto"+
+                    "\n\n Esto es otro mensaje")
+    print("Correo enviado exitosamente")  
+except smtplib.SMTPException as e:
+    print(f"Error al mandar el correo {e}")
+finally:
+    conec.quit()
